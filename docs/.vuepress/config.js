15c264563e559a4170ed337d125e017be09663fa
KimWash <ckm0728wash@gmail.com>
2022-01-21 02:44:25 +0900

    Post up!

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 const path = require("path")
 
 module.exports = {
-  title: 'kimwash.github.io',
-  description: 'Awesome description',
+  title: 'Wh@t !s Development?',
+  description: '배워가고 있는 현업..? 대학생 개발자',
   markdown: {
     anchor:{
       permalink: false, permalinkBefore: false, permalinkSymbol: ''
