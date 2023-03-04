c514164d4f705015a89c700fb6aa055b31853129
KimWash <ckm0728wash@gmail.com>
2023-03-04 22:40:58 +0900

    FEAT: Implement random card color

--- docs/.vuepress/theme/components/PostCard.vue
+++ docs/.vuepress/theme/components/PostCard.vue
@@ -1,5 +1,9 @@
 <template>
-  <article @click="onClick" class="tile is-child box">
+  <article
+    @click="onClick"
+    class="tile is-child box"
+    v-bind:style="`background: linear-gradient(45deg, ${bgColors[0]} 0%, ${bgColors[1]} 100%)`"
+  >
     <p class="title">
       {{ title }}
     </p>
@@ -12,6 +16,11 @@
 </template>
 
 <script>
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
 export default {
   props: {
     title: {
@@ -27,6 +36,23 @@ export default {
       default: "게시일을 불러올 수 없습니다.",
     },
   },
+  data: () => {
+    return {
+      bgColors: [
+        ["#FF7495", "#FF9A74"],
+        ["#FF4973", "#FE1B50"],
+        ["#ec008c", "#fc6767"],
+        ["#f953c6", "#b91d73"],
+        ["#FF416C", "#FF4B2B"],
+      ].find((_, idx, arr) => idx == getRandomInt(0, arr.length)) ?? [
+        "#FF512F",
+        "#DD2476",
+      ],
+    };
+  },
+  created() {
+    console.log(this.bgColors);
+  },
   methods: {
     onClick() {
       this.$emit("onClick");
@@ -39,7 +65,6 @@ export default {
 .is-child {
   border-radius: 18px;
   color: white;
-  background: linear-gradient(45deg, #327ae7 0%, #6bd0ff 100%);
 }
 </style>
 <style scoped>
